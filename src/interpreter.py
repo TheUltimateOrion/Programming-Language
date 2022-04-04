c88f748aa8617d4ab8682ca7785071981317dec0
TheUltimateOrion <aaryanbanerje@gmail.com>
2022-04-04 14:27:30 -0400

    Minor changes

--- src/interpreter.py
+++ src/interpreter.py
@@ -1334,9 +1334,7 @@ def run(fn, text):
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
     if error: return None, error
-
-    print(tokens)
-
+    
     # Generate AST
     parser = Parser(tokens)
     ast = parser.parse()
